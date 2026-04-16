25698385e2b2902703675a09793cb7e8707501fd
Zahraa Salloum <zahraa.h.salloum@gmail.com>
2026-04-16 14:51:55 +0300

    Rename CryptoStream to CryptoSecure

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'message' => 'CryptoStream API',
+        'message' => 'CryptoSecure API',
         'version' => '1.0.0',
         'status' => 'active',
     ]);
